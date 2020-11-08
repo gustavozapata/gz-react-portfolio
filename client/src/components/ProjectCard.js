0001ff181e7856686fae47c897f95ce0561258f0
Gustavo Zapata <tavordie@hotmail.com>
2020-11-08 10:48:17 +0000

    fix skills layout + footer + projects

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./styles/ProjectCard.css";
-import {convertToImageName} from "../utils/utils"
+import { convertToImageName } from "../utils/utils";
 
-export default function ProjectCard({project}) {
-  const {name, type, technologies} = project
+export default function ProjectCard({ project }) {
+  const { name, type, technologies } = project;
   return (
     <div className="ProjectCard">
       <p className="title">{name}</p>
       <p className="type-project">{type}</p>
       {technologies.map((tech, i) => (
         <div className="tech" key={i}>
-        <img src={require(`../images/${convertToImageName(tech)}.png`)} alt={`${tech} Icon`}/>
-        <p>{tech}</p>
-      </div>
+          <img
+            src={require(`../images/${convertToImageName(tech)}.png`)}
+            alt={`${tech} Icon`}
+          />
+          <p>{tech}</p>
+        </div>
       ))}
     </div>
   );
