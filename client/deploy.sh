54b0054a43fdefd972d8bef6fda33dfea768c9ec
Gustavo Zapata <tavordie@hotmail.com>
2021-11-13 02:04:38 +0000

    comment out rm all files but .git on deploy.sh and update projects.json

--- client/deploy.sh
+++ client/deploy.sh
@@ -17,7 +17,7 @@ echo "\033[1;32mworking on client...\033[0m"
 npm run build
 cd build
 shopt -s extglob
-rm -rf !(.git) #delete all folders but .git
+# rm -rf !(.git) #delete all folders but .git | Update 13 Nov 2021: comment this line for now since delete everything including git on mac
 addncommit
 git push
 echo "\033[1;32mClient (cPanel) repo done \033[0m"
