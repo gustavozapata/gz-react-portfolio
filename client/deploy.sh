e8e14a0aceff3a77438b94a712708986ab7a98d5
Gustavo Zapata <tavordie@hotmail.com>
2021-12-17 00:48:34 +0000

    update error comment on deploy.sh

--- client/deploy.sh
+++ client/deploy.sh
@@ -17,7 +17,7 @@ echo "\033[1;32mworking on client...\033[0m"
 npm run build
 cd build
 shopt -s extglob
-# rm -rf !(.git) #delete all folders but .git | Update 13 Nov 2021: comment this line for now since delete everything including git on mac
+# rm -rf !(.git) #delete all folders but .git | Update 13 Nov 2021: comment this line for now since it's deleting everything including .git folder on mac -> I think this script doesn't work on mac the way it used to work on windows
 addncommit
 git push
 echo "\033[1;32mClient (cPanel) repo done \033[0m"
