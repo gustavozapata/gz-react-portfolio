724c1bc057749e41e779a09ac726410a21ac7c13
Gustavo Zapata <tavordie@hotmail.com>
2022-06-16 09:32:45 +0100

    update years of experience and fullstack engineer

--- src/content/English.js
+++ src/content/English.js
@@ -1,10 +1,10 @@
 export const EN_Content = {
   TITLE: "I’m passionate about the design and development of computer programs",
-  SUB_TITLE: "FULLSTACK DEVELOPER",
+  SUB_TITLE: "FULLSTACK ENGINEER",
   SKILLS_TITLE: "Skills",
   SKILLS_SUB:
-    "Technical skills gained during my 6 years in software engineering",
-  SKILLS_DESC: "Skills gained during 6 years in",
+    "Technical skills gained during my 8 years in software engineering",
+  SKILLS_DESC: "Skills gained during 8 years in",
   SKILLS_HIGHLIGHT: "professional and technical skills.",
   EDUCATION_TITLE: "Education",
   EDUCATION_SUB: "My most recent Education",
